6ddc8ee0ae91bd217eed023632fca201e2043c43
Joseph Abel <josephabel540@gmail.com>
2025-11-23 22:26:08 +0100

    commit update

--- Models/userModel.js
+++ Models/userModel.js
@@ -28,14 +28,12 @@ const UserSchema = new mongoose.Schema({
         type: Date, 
         default: Date.now 
     },
-    // Remove the properties array completely
-    // properties: [userPropertySchema]
+   
 }, {
     timestamps: true
 });
 
-UserSchema.index({ email: 1 });
-UserSchema.index({ username: 1 });
+
 
 const User = mongoose.model('User', UserSchema);
 module.exports = User;
