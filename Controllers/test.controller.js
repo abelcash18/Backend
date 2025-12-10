89b817ae9b9dc8a0c02943b2091ffa16e2a9966b
Joseph Abel <josephabel540@gmail.com>
2025-12-10 17:14:33 +0100

    updated new

--- Controllers/test.controller.js
+++ Controllers/test.controller.js
@@ -7,19 +7,19 @@ res.status(200).json({message: "You are authenticated"});
 };
 
  
-// //exports.shouldBeAdmin = async (req, res)=>{
-//     const token = req.cookies.token;
+exports.shouldBeAdmin = async (req, res)=>{
+     const token = req.cookies.token;
 
-// if(!token) return res.status(401).json({message:"Not Authenticated!"});
+ if(!token) return res.status(401).json({message:"Not Authenticated!"});
 
-// jwt.verify(token, process.env.JWT_SECRET_KEY, async(err, payload)=>{
-// if (err) return res.status(403).json({message:" Token is not Valid!"});
-// if (!payload.isAdmin){
-//     return res.status(403).json({message:" Not authorized!"});
-// };
-// });
-// res.status(200).json({message: "You are authenticated"});
-// };
+ jwt.verify(token, process.env.JWT_SECRET_KEY, async(err, payload)=>{
+ if (err) return res.status(403).json({message:" Token is not Valid!"});
+ if (!payload.isAdmin){
+  return res.status(403).json({message:" Not authorized!"});
+ };
+ });
+ res.status(200).json({message: "You are authenticated"});
+ };
 
 
  
