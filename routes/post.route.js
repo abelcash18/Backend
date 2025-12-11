b478d274bb0a5b4160f13566b146760acdd1b69e
Joseph Abel <josephabel540@gmail.com>
2025-12-11 07:46:21 +0100

    working now

--- routes/post.route.js
+++ routes/post.route.js
@@ -6,9 +6,9 @@ const router = express.Router();
 
 router.get("/",propertyController.getPosts );
 router.get("/:id",propertyController.getPost);
-router.post("/", verifyToken,propertyController.addPost);
+router.post("/", propertyController.addPost);
 router.put("/:id", propertyController.updatePost);
-router.delete("/:id", verifyToken, propertyController.deletePost);
+router.delete("/:id", propertyController.deletePost);
 router.get("/user/:userId", propertyController.getUserPosts);
 
 
--- routes/user.route.js
+++ routes/user.route.js
@@ -9,7 +9,7 @@ const router = express.Router();
 router.get("/", userController.getUsers);
 router.get("/:id",userController.getUser);
 router.put("/:id", userController.updateUser);
-router.delete("/:id", verifyToken, userController.deleteUser);
+router.delete("/:id", userController.deleteUser);
 
 
 module.exports = router;
