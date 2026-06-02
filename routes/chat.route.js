d6f64566ff236fab4de07208e51642b50ddf2426
Joseph Abel <josephabel540@gmail.com>
2026-06-02 12:27:08 +0100

    checking

--- routes/chat.route.js
+++ routes/chat.route.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const { verifyToken } = require('../Middleware/verifyToken.js');
 const chatController = require('../Controllers/chatController');
 
 const router = express.Router();
@@ -8,9 +7,9 @@ router.post("/client/start", chatController.startChat);
 router.get("/client/:chatId", chatController.getClientChat);
 router.post("/client/:chatId/message", chatController.sendClientMessage);
 
-router.get("/owner/chats", verifyToken, chatController.getOwnerChats);
-router.get("/owner/chat/:chatId", verifyToken, chatController.getOwnerChat);
-router.post("/owner/chat/:chatId/message", verifyToken, chatController.sendOwnerMessage);
-router.put("/owner/chat/:chatId/read", verifyToken, chatController.markAsRead);
+router.get("/owner/chats", chatController.getOwnerChats);
+router.get("/owner/chat/:chatId", chatController.getOwnerChat);
+router.post("/owner/chat/:chatId/message", chatController.sendOwnerMessage);
+router.put("/owner/chat/:chatId/read", chatController.markAsRead);
 
 module.exports = router;
