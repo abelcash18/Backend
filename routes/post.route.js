fcec3c70b9e209b4b9d01e388826f7a15dfc34ef
Joseph Abel <josephabel540@gmail.com>
2026-07-04 00:16:36 +0100

    edd

--- routes/post.route.js
+++ routes/post.route.js
@@ -4,13 +4,16 @@ const propertyController = require ('../Controllers/propertyController')
 
 const router = express.Router();
 
-router.get("/",propertyController.getPosts );
-router.get("/:id",propertyController.getPost);
-router.post("/", propertyController.addPost);
-router.put("/:id", propertyController.updatePost);
-router.delete("/:id", propertyController.deletePost);
+router.get("/", propertyController.getPosts);
+router.get("/:id", propertyController.getPost);
+
+// PASTED FIX: Added verifyToken here so req.userId becomes available
+router.post("/", verifyToken, propertyController.addPost);
+
+// OPTIONAL FIX: You will likely need verifyToken here too so users can only update/delete their own posts
+router.put("/:id", verifyToken, propertyController.updatePost);
+router.delete("/:id", verifyToken, propertyController.deletePost);
+
 router.get("/user/:userId", propertyController.getUserPosts);
 
-
-
-module.exports = router;  
+module.exports = router;
