812a5744959a81b508ec60b2ec41b5f54cbcdf8c
Joseph Abel <josephabel540@gmail.com>
2026-07-07 16:54:48 +0100

    fix

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const authController = require('../controllers/auth.controller.js');
 const { register, login, logout } = require("../controllers/auth.controller.js");
 const { forgotPassword, resetPassword, verifyResetToken } = require("../controllers/resetPassword.js");
 
